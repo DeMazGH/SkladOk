399ec703a9866bf719d7225c1187108088d5f344
Denis Maza <demaz@bk.ru>
2025-03-31 21:50:28 +0700

    Added documentation for service methods.

--- src/main/java/org/example/skladok/service/impl/SocksServiceImpl.java
+++ src/main/java/org/example/skladok/service/impl/SocksServiceImpl.java
@@ -23,6 +23,13 @@ public class SocksServiceImpl implements SocksService {
         this.socksRepository = socksRepository;
     }
 
+    /**
+     * Метод регистрирует прибытие носков на склад. Если носков с характеристиками переданными в запросе нет на складе,
+     * то будет создана новая запись в БД, в противном случае количество носков суммируется. itemColor не учитывает
+     * регистр. Метод потокобезопасный.
+     *
+     * @param socksDto характеристики партии носков.
+     */
     @Override
     @Transactional(isolation = Isolation.SERIALIZABLE)
     public void addToStorage(SocksDto socksDto) {
@@ -38,9 +45,18 @@ public class SocksServiceImpl implements SocksService {
         }
     }
 
+    /**
+     * Метод регистрирует убытие носков со склада. Если носков с характеристиками переданными в запросе нет на складе
+     * или их количество недостаточно, то будет выброшено исключение. itemColor не учитывает
+     * регистр. Метод потокобезопасный.
+     *
+     * @param socksDto характеристики партии носков.
+     *
+     * @throws NotEnoughSocksException при недостаточном количестве носков на складе.
+     */
     @Override
     @Transactional(isolation = Isolation.SERIALIZABLE)
-    public void retrieveFromStorage(SocksDto socksDto) {
+    public void retrieveFromStorage(SocksDto socksDto) throws NotEnoughSocksException {
         Socks socksInStock = socksRepository
                 .findSocksByItemColorIgnoreCaseAndMaterialPercentage(socksDto.itemColor(), socksDto.materialPercentage());
 
@@ -52,6 +68,15 @@ public class SocksServiceImpl implements SocksService {
         socksRepository.save(socksInStock);
     }
 
+    /**
+     * Метод возвращает количество носков оставшихся на складе. itemColor не учитывает регистр.
+     *
+     * @param itemColor          цвет носков.
+     * @param compareType        тип оператора сравнения для значения количества материала в составе носков.
+     * @param materialPercentage значение процента материала в составе носков.
+     *
+     * @return общее количество носков на складе.
+     */
     @Override
     public Long getSocksQuantity(String itemColor, String compareType, Integer materialPercentage) {
 
